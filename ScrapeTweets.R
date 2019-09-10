adfc663394ff0e1483bdbdbf223ce9bba99d3b69
Fuller <samf1021@iastate.edu>
2019-09-10 14:51:02 -0500

    Set up script to retroactively look at tweets leading up to debate, and to stream tweets during debates.

--- ScrapeTweets.R
+++ ScrapeTweets.R
@@ -4,14 +4,31 @@
 library(rtweet)
 library(readr)
 
-##Load Candidate Data
-CandidateTwitterHandles <- read_csv("CandidateTwitterHandles.csv")
-handles<-factor(CandidateTwitterHandles$`Twitter Handle`)
-handles<-paste0("@",as.character(CandidateTwitterHandles$`Twitter Handle`))
+##query terms
+q<-c("Joe Biden", "Cory Booker", "Pete Buttigieg", "Julián Castro", "Kamala Harris", 
+     "Amy Klobuchar","Beto O’Rourke","Bernie Sanders", "Elizabeth Warren","Andrew Yang",
+     "Biden", "Booker", "Buttigieg", "Castro", "Harris", "Klobuchar", "O'Rourke", "Sanders", "Warren", "Yang",
+     "@SenSanders","@ewarren", "@BetoORourke","@AndrewYang", "@JulianCastro",
+     "@CoryBooker", "@SenKamalaHarris", "@amyklobuchar", "@JoeBiden", "@PeteButtigieg" ,"@kamalaHarris",
+     "@BernieSanders","@SenWarren")
 
-##Scrape Candidate Tweets
-##THIS TAKES A LONG TIME AND YOU CAN NOT LET YOUR SCREEN GO DARK
-  
-cantweets<-get_timelines(hand, n = 2000, retryonratelimit = TRUE)##Type some rediculous number here to get all tweets
-tweets<-subset(cantweets, select =c("created_at", "screen_name","text"))
-write.csv(tweets, "testtweets.csv")
+d<-c("Joe Biden", "Cory Booker", "Pete Buttigieg", "Julián Castro", "Kamala Harris", 
+     "Amy Klobuchar","Beto O’Rourke","Bernie Sanders", "Elizabeth Warren","Andrew Yang",
+     "Biden", "Booker", "Buttigieg", "Castro", "Harris", "Klobuchar", "O'Rourke", "Sanders", "Warren", "Yang",
+     "@SenSanders","@ewarren", "@BetoORourke","@AndrewYang", "@JulianCastro",
+     "@CoryBooker", "@SenKamalaHarris", "@amyklobuchar", "@JoeBiden", "@PeteButtigieg" ,"@kamalaHarris",
+     "@BernieSanders","@SenWarren",
+     "Joe", "Cory", "Pete", "Julián", "Julian", "Kamala", "Amy", "Beto", "Bernie", "Elizabeth", "Andrew")  
+
+##Scrape tweets about candidtes
+##PRE DEBATE
+
+pre<-Map(
+  "search_tweets",q,
+  n = 1000
+)
+
+ds <- do_call_rbind(pre)
+##DUring Debate
+
+tweets_about_candidates<-stream_tweets("@SenSanders")
